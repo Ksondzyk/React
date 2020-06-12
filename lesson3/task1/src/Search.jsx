5f874caa0d3661f6a81cab9cb929b1357324d70d
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-12 18:22:05 +0300

    react component

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -9,7 +9,7 @@ const Search = (props) => {
   console.log(props);
   return (
     <div className="search">
-      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+      <h1 className="search__title">Hello, Tom. What to search for you?</h1>
       <div className="search__field">
         <input type="text" className="search__input" />
         <button className="search__button">Search</button>
