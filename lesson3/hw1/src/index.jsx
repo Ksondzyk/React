f14d79e06d220fed7297f80321868168cc097cec
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-13 20:26:48 +0300

    react component

--- lesson3/hw1/src/index.jsx
+++ lesson3/hw1/src/index.jsx
@@ -9,10 +9,19 @@ const userData = {
   firstName: "John",
   lastName: "Doe",
   birthDate() {
-    return (
-      new Date().getFullYear() -
-      new Date("1991-01-17T11:11:11.819Z").getFullYear()
-    );
+    const now = new Date(); //Текущя дата
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate()); //Текущя дата без времени
+    const dob = new Date("2001-01-01T11:11:11.819Z"); //Дата рождения
+    const dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate()); //ДР в текущем году
+    let age; //Возраст
+
+    //Возраст = текущий год - год рождения
+    age = today.getFullYear() - dob.getFullYear();
+    //Если ДР в этом году ещё предстоит, то вычитаем из age один год
+    if (today < dobnow) {
+      age = age - 1;
+    }
+    return age;
   },
 };
 
