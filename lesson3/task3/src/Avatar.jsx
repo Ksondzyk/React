4ece7c7576b68418dbcb3954f691c01d8dfea12f
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-13 09:55:18 +0300

    push

--- lesson3/task3/src/Avatar.jsx
+++ lesson3/task3/src/Avatar.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 const Avatar = (props) => {
   console.log(props);
-  return (
-    <img className="avatar" src={props.user.avatarUrl} alt={props.user.name} />
-  );
+  return <img className="avatar" src={props.user} alt={props.user} />;
 };
 export default Avatar;
