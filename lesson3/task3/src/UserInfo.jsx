d68e12e56250634f282c5ec628ffc67968a0587c
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-13 10:13:33 +0300

    push

--- lesson3/task3/src/UserInfo.jsx
+++ lesson3/task3/src/UserInfo.jsx
@@ -5,8 +5,8 @@ const UserInfo = (props) => {
   console.log(props);
   return (
     <div className="user-info">
-      <Avatar user={props.author} name={props.author} />
-      <div className="user-info__name">{props.author.name}</div>
+      <Avatar user={props.user} name={props.user} />
+      <div className="user-info__name">{props.user.name}</div>
     </div>
   );
 };
