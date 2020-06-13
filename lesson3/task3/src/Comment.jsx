51e3e8e1702b85dbfbb45c63ec6a4c2692c1eb35
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-13 11:26:12 +0300

    push

--- lesson3/task3/src/Comment.jsx
+++ lesson3/task3/src/Comment.jsx
@@ -8,8 +8,8 @@ const formatDate = (date) => moment(date).format("DD MMM YYYY");
 function Comment(props) {
   console.log(props);
   return (
-    <div className="comment" author={props.user}>
-      <UserInfo user={props.user} />
+    <div className="comment">
+      <UserInfo user={props.author} />
       <div className="comment__text">{props.text}</div>
       <div className="comment__date">{formatDate(props.date)}</div>
     </div>
