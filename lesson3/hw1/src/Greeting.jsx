2b99b32af477d0544dc16a9547f0e2fa21428d8e
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-13 16:51:25 +0300

    push

--- lesson3/hw1/src/Greeting.jsx
+++ lesson3/hw1/src/Greeting.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import moment from "moment";
 
+const formatDate = (date) => moment(date).format("YYYY");
 const Greeting = (props) => {
   return (
     <div className="greeting">
       {`My name is ${props.user.firsName} ${
         props.user.lastName
-      }. I'm ${props.user.birthDate()} years old`}
+      }. I'm ${formatDate(props.user.birthDate)} years old`}
     </div>
   );
 };
--- lesson3/hw1/src/index.jsx
+++ lesson3/hw1/src/index.jsx
@@ -5,18 +5,23 @@ import Greeting from "./Greeting";
 
 const elementRoot = document.querySelector("#root");
 
+// const user = {
+//   firsName: "John",
+//   lastName: "Doe",
+//   birthDate() {
+//     const birhdayDate = new Date("2001-01-01T11:11:11.819Z").getFullYear();
+//     const dayNow = new Date().getFullYear();
+//     const result = dayNow - birhdayDate;
+//     return result;
+//   },
+// };
+
 const user = {
   firsName: "John",
   lastName: "Doe",
-  birthDate() {
-    const birhdayDate = new Date("2001-01-01T11:11:11.819Z").getFullYear();
-    const dayNow = new Date().getFullYear();
-    const result = dayNow - birhdayDate;
-    return result;
-  },
+  birthDate: "2001-01-01T11:11:11.819Z",
 };
-
-const birhdayDate =
-  new Date().getFullYear() - new Date("2003-01-01T11:11:11.819Z").getFullYear();
+// const birhdayDate =
+//   new Date().getFullYear() - new Date("2003-01-01T11:11:11.819Z").getFullYear();
 
 ReactDOM.render(<Greeting user={user} />, elementRoot);
