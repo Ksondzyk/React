1cf0728fa0aa44c4b948f53fdfe8e23116046d8f
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-15 10:46:05 +0300

    React component

--- lesson3/hw2/src/ProfileBirth.jsx
+++ lesson3/hw2/src/ProfileBirth.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import moment from "moment";
 
 const formatDate = (date) => moment(date).format("DD MMM YY");
-const BrofileBirth = (props) => {
+const ProfileBirth = (props) => {
   return (
     <div className="profile__birth">
       {`Was born ${formatDate(props.date)} in ${props.city}`}
     </div>
   );
 };
-export default BrofileBirth;
+export default ProfileBirth;
