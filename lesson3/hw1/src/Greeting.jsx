30734608c4a9c5a592ae86dd3ec994e63e056525
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-15 10:14:12 +0300

    push

--- lesson3/hw1/src/Greeting.jsx
+++ lesson3/hw1/src/Greeting.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import moment from "moment";
 
-// const formatDate = (date) => moment(date).format("YY") - new Date().getFullYear;
+const age = (birthDate) => {
+  const YearMs = 31536000000;
+  return Math.floor((new Date() - new Date(birthDate)) / YearMs);
+};
+
 const Greeting = (props) => {
   return (
     <div className="greeting">
-      {`My name is ${props.user.firstName} ${
-        props.user.lastName
-      }. I'm ${props.user.birthDate()} years old`}
+      {`My name is ${props.firstName} ${props.lastName}. I'm ${age(
+        props.birthDate
+      )} years old`}
     </div>
   );
 };
--- lesson3/hw1/src/index.jsx
+++ lesson3/hw1/src/index.jsx
@@ -5,15 +5,22 @@ import Greeting from "./Greeting";
 
 const elementRoot = document.querySelector("#root");
 
-const userData = {
-  firstName: "John",
-  lastName: "Doe",
-  birthDate() {
-    const YEAR_IN_MILLISECONDS = 31536000000;
-    return Math.floor(
-      (new Date() - new Date("2001-01-01T11:11:11.819Z")) / YEAR_IN_MILLISECONDS
-    );
-  },
-};
+// const userData = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   birthDate() {
+//     const YEAR_IN_MILLISECONDS = 31536000000;
+//     return Math.floor(
+//       (new Date() - new Date("2001-01-01T11:11:11.819Z")) / YEAR_IN_MILLISECONDS
+//     );
+//   },
+// };
 
-ReactDOM.render(<Greeting user={userData} />, elementRoot);
+ReactDOM.render(
+  <Greeting
+    firstName="John"
+    lastName="Doe"
+    birthDate={new Date("2001-01-01T11:11:11.819Z")}
+  />,
+  elementRoot
+);
