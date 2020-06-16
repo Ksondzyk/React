510517c192d257e920ac96312409aaaa43b229dd
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-16 17:09:32 +0300

    text content

--- lesson5/hw1/src/Toggler.jsx
+++ lesson5/hw1/src/Toggler.jsx
@@ -4,8 +4,9 @@ const RED = "#f00";
 const GREEN = "#0f0";
 const BLUE = "#00f";
 
-class Toggle extends React.Component {
-  constructor(props) {
+class Toggler extends React.Component {
+  constructor(props, e) {
+    console.log(props);
     super(props);
     this.state = {
       btnSwith: "off",
@@ -13,33 +14,38 @@ class Toggle extends React.Component {
   }
 
   setBodyColor = (color, e) => {
-    console.log(e);
+    // console.log(e);
     document.body.style.backgroundColor = color;
-    e.target.textContent = "On";
-    this.setState({
-      btnSwith: "On",
-    });
+
+    console.log(React.textContent);
+
+    if (e.target.textContent === "off") {
+      this.setState({
+        btnSwith: (this.state.btnSwith = "On"),
+      });
+    }
+
     // console.log(btnSwith);
   };
   render() {
     return (
-      <div className="colors">
+      <div className="toggler">
         <div
-          className="colors__button"
+          className="toggler__button"
           style={{ backgroundColor: RED }}
           onClick={this.setBodyColor.bind(this, RED)}
         >
           {this.state.btnSwith}
         </div>
         <div
-          className="colors__button"
+          className="toggler__button"
           style={{ backgroundColor: GREEN }}
           onClick={this.setBodyColor.bind(this, GREEN)}
         >
           {this.state.btnSwith}
         </div>
         <div
-          className="colors__button"
+          className="toggler__button"
           style={{ backgroundColor: BLUE }}
           onClick={this.setBodyColor.bind(this, BLUE)}
         >
@@ -50,4 +56,4 @@ class Toggle extends React.Component {
   }
 }
 
-export default Toggle;
+export default Toggler;
--- lesson5/hw1/src/index.jsx
+++ lesson5/hw1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import Toggle from "./Toggle";
+import Toggler from "./Toggler";
 
 const elementRoot = document.querySelector("#root");
 
-ReactDOM.render(<Toggle />, elementRoot);
+ReactDOM.render(<Toggler />, elementRoot);
