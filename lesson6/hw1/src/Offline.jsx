cd8d9face3a2d9bc64b18802d39898c8fdbbc77a
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-18 18:37:55 +0300

    React component logic

--- lesson6/hw1/src/Offline.jsx
+++ lesson6/hw1/src/Offline.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Offline = (props) => {
-  console.log(props);
   return <span className="status__text">{props.text}</span>;
 };
 
--- lesson6/hw1/src/Online.jsx
+++ lesson6/hw1/src/Online.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Online = (props) => {
-  console.log(props);
   return <span className="status__text">{props.text}</span>;
 };
 
