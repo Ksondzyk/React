707dc7f2d4557a115a0dbeda6fce41e3868298db
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-18 13:48:16 +0300

    React if esse

---
+++ lesson6/task1/src/Auth.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import Greeting from "./Greeting";
+
+class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+  handleLogin = () => {
+    this.setState({
+      isLoggedIn: true,
+    });
+  };
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+    });
+  };
+  render() {
+    // const button = this.state.isLoggedIn ? (
+    //   <button className="btn logout" onClick={this.handleLogout}>
+    //     Logout
+    //   </button>
+    // ) : (
+    //   <button className="btn login" onClick={this.handleLogin}>
+    //     Login
+    //   </button>
+    // );
+
+    // if (this.state.isLoggedIn) {
+    //   button = (
+    //     <button className="btn logout" onClick={this.handleLogin}>
+    //       Logout
+    //     </button>
+    //   );
+    // } else {
+    //   button = (
+    //     <button className="btn login" onClick={this.handleLogin}>
+    //       Login
+    //     </button>
+    //   );
+    // }
+    return (
+      <div className="panel">
+        <Greeting isLoggedIn={this.state.isLoggedIn} />
+        {this.state.isLoggedIn ? (
+          <button className="btn logout" onClick={this.handleLogout}>
+            Logout
+          </button>
+        ) : (
+          <button className="btn login" onClick={this.handleLogin}>
+            Login
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Auth;
