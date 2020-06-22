b142712d27eb24f43032afc749a388f0cc59118e
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-22 18:42:14 +0300

    react key in ul list

--- lesson7/hw1/src/Transaction.jsx
+++ lesson7/hw1/src/Transaction.jsx
@@ -5,13 +5,14 @@ const formatDate = (date) => moment(date).format("DD MMM ");
 const formatTime = (time) => moment(time).format("HH:mm");
 
 const Transaction = ({ from, to, amount, rate, time }) => {
+  const number = new Intl.NumberFormat("en-GB").format(amount);
   return (
     <li className="transaction">
       <span className="transaction__date">{formatDate(time)}</span>
       <span className="transaction__time">{formatTime(time)}</span>
       <span className="transaction__assets">{`${from} → ${to}`}</span>
       <span className="transaction__rate">{rate}</span>
-      <span className="transaction__amount">{amount}</span>
+      <span className="transaction__amount">{number}</span>
     </li>
   );
 };
