f6d4bf22519b65df819d01b11597dd8537ce2db2
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-22 15:28:07 +0300

    react key in ul list

---
+++ lesson7/task2/src/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import UserList from "./UserList";
+
+const elemRoot = document.querySelector("#root");
+const users = [
+  {
+    age: 21,
+    name: "Bob",
+    id: "id-0",
+  },
+  {
+    age: 17,
+    name: "Tom",
+    id: "id-1",
+  },
+  {
+    age: 18,
+    name: "Tom",
+    id: "id-2",
+  },
+  {
+    age: 18,
+    name: "Tad",
+    id: "id-3",
+  },
+  {
+    age: 45,
+    name: "Sam",
+    id: "id-4",
+  },
+];
+
+ReactDOM.render(<UserList users={users} />, elemRoot);
