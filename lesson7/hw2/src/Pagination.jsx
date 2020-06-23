24701fb283785e75fa91772dbc5907031a80dfd7
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-23 11:07:02 +0300

    push

--- lesson7/hw2/src/Pagination.jsx
+++ lesson7/hw2/src/Pagination.jsx
@@ -5,7 +5,7 @@ const Pagination = ({ currentPage, goNext, goPrev, totalItems }) => {
   let isNextPageAvailable = "→";
   let disabledNext = false;
   let disabledPrev = false;
-  if (currentPage === totalItems) {
+  if (totalItems === 3) {
     disabledNext = true;
     isNextPageAvailable = "";
   } else {
@@ -13,7 +13,7 @@ const Pagination = ({ currentPage, goNext, goPrev, totalItems }) => {
     isNextPageAvailable = "→";
   }
 
-  if (currentPage === 1) {
+  if (totalItems !== 3) {
     disabledPrev = true;
     isPrevPageAvailable = "";
   } else {
