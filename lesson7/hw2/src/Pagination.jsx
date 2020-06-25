117a09cf1c1b0da4e12a0561789d1bc011cf77e9
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-25 11:06:30 +0300

    push

--- lesson7/hw2/src/Pagination.jsx
+++ lesson7/hw2/src/Pagination.jsx
@@ -7,10 +7,16 @@ const Pagination = ({
   totalItems,
   itemsPerPage,
 }) => {
-  console.log(totalItems);
-  const isPrevPageAvailable = currentPage > 0;
+  // const isPrevPageAvailable = !!(
+  //   currentPage === 0 && currentPage !== totalItems
+  // );
+
+  // const isNextPageAvailable = currentPage === totalItems;
+
+  const isPrevPageAvailable = currentPage === 0;
   const isNextPageAvailable =
     currentPage < Math.ceil(totalItems / itemsPerPage) - 1;
+  console.log(isPrevPageAvailable, isNextPageAvailable);
 
   return (
     <div className="pagination">
@@ -18,8 +24,8 @@ const Pagination = ({
         {isPrevPageAvailable ? "" : "←"}
       </button>
       <span className="pagination__page">{currentPage + 1}</span>
-      <button className="btn" disabled={isNextPageAvailable} onClick={goNext}>
-        {isNextPageAvailable ? "" : "→"}
+      <button className="btn" disabled={!isNextPageAvailable} onClick={goNext}>
+        {isNextPageAvailable ? "→" : ""}
       </button>
     </div>
   );
