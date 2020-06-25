6988546d2084345b25dd84ea03e98535f82ba3c2
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-25 20:29:40 +0300

    push

--- lesson8/task3/src/Dimensions.jsx
+++ lesson8/task3/src/Dimensions.jsx
@@ -8,12 +8,12 @@ class Dimensions extends Component {
 
   componentDidMount() {
     window.addEventListener("resize", this.onResize);
-    this.state = {
-      width: innerWidth,
-      height: innerHeight,
-    };
+    // this.state = {
+    //   width: innerWidth,
+    //   height: innerHeight,
+    // };
     const { innerWidth, innerHeight } = window;
-    document.title = `${innerWidth} x ${innerHeight}`;
+    // document.title = `${innerWidth} x ${innerHeight}`;
     this.setDimensions(innerWidth, innerHeight);
   }
 
