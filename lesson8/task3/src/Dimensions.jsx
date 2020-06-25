720c9304c45df3a304c01013813b3e6e7cd6c91f
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-25 20:27:04 +0300

    componentWillMount, componentDidMount

---
+++ lesson8/task3/src/Dimensions.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Dimensions extends Component {
+  state = {
+    width: null,
+    height: null,
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.onResize);
+    this.state = {
+      width: innerWidth,
+      height: innerHeight,
+    };
+    const { innerWidth, innerHeight } = window;
+    document.title = `${innerWidth} x ${innerHeight}`;
+    this.setDimensions(innerWidth, innerHeight);
+  }
+
+  componentWillMount() {
+    window.addEventListener("resize", this.onResize);
+  }
+
+  onResize = (e) => {
+    const { innerWidth, innerHeight } = e.target;
+    this.setDimensions(innerWidth, innerHeight);
+  };
+
+  setDimensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+
+    document.title = `${innerWidth} x ${innerHeight}`;
+  };
+  render() {
+    return (
+      <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>
+    );
+  }
+}
+
+export default Dimensions;
