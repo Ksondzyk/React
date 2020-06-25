d4ae70cc47d5d04cdba6f5b4cc37ead90bc9433c
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-25 14:05:33 +0300

    React life cycle methods

---
+++ lesson8/task1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Demo from "./Demo";
+
+const elemRoot = document.querySelector("#root");
+
+ReactDOM.render(<Demo />, elemRoot);
