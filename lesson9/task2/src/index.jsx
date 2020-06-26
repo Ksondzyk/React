809c4e2dd04f6bf5ae058e0389941ac078e07b0b
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-26 19:54:06 +0300

    push

---
+++ lesson9/task2/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import UserForm from "./UserForm";
+
+const elementRoot = document.querySelector("#root");
+
+ReactDOM.render(<UserForm />, elementRoot);
+
