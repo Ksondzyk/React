d220da8196dea154512f903f1a23fc8d9b462e5f
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-26 18:52:10 +0300

    React form

---
+++ lesson9/task1/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Search from "./Search";
+
+const elementRoot = document.querySelector("#root");
+
+ReactDOM.render(<Search />, elementRoot);
+
