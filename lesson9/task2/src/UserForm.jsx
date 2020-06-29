5b9ea38ece52f0a7208c543116ecc2fef90ce38a
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-29 15:08:30 +0300

    push

--- lesson9/task2/src/UserForm.jsx
+++ lesson9/task2/src/UserForm.jsx
@@ -28,7 +28,7 @@ class UserForm extends Component {
 
   render() {
     return (
-      <form className="login-form" onSubmit={() => this.props.onSubmit()}>
+      <form className="login-form" onSubmit={this.handleSumbit}>
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
