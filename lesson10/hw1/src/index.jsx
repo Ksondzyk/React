ffbae0429555fe703afeb5eff14c1d300af4c2af
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-02 14:16:18 +0300

    calculate

---
+++ lesson10/hw1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Calculator from "./Calculator";
+
+const elementRoot = document.querySelector("#root");
+
+ReactDOM.render(<Calculator userId="github" />, elementRoot);
