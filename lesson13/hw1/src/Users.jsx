556908b49f1ca778008c00a811b1654e09baa743
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-20 18:24:12 +0300

    push

--- lesson13/hw1/src/Users.jsx
+++ lesson13/hw1/src/Users.jsx
@@ -17,7 +17,7 @@ const Users = ({ match }) => {
       </ul>
       <Switch>
         <Route exact path={match.url}>
-          <span>Select a product please</span>
+          <span>Select a user please</span>
         </Route>
         <Route path={`${match.url}/:productId`}>
           <Product />
