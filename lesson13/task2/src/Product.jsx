3288ce6a2ea4083c7ad6bdd538215a6f56d5eb75
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-20 17:57:04 +0300

    push

--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -3,6 +3,6 @@ import { useParams } from "react-router-dom";
 
 const Product = () => {
   const { productId } = useParams();
-  return <div className="product">{`Product ${productId}`}</div>;
+  return <div className="product">{`${productId}`}</div>;
 };
 export default Product;
