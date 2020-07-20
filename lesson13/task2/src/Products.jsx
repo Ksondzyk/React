f64e8539acebdf15a7b6e5d4f46413f4b4268737
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-20 17:48:23 +0300

    React Router

---
+++ lesson13/task2/src/Products.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import Product from "./Product";
+
+const Products = ({ match }) => {
+  console.log(match);
+  return (
+    <div className="page__content">
+      <ul className="navigation">
+        <li className="navigation__item">
+          <Link to={`${match.url}/book`}>Book</Link>
+        </li>
+        <li className="navigation__item">
+          <Link to={`${match.url}/ball`}>Ball</Link>
+        </li>
+      </ul>
+      <h1>Products</h1>
+      <Switch>
+        <Route exact path={match.url}>
+          <span>Select a product pleace</span>
+        </Route>
+        <Route path={`${match.url}/:productId`}>
+          <Product />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+export default Products;
