b76b5396a247a3e09e4a1284e668b477393bbbdc
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-20 18:22:18 +0300

    React router

---
+++ lesson13/hw1/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import Users from "./Users";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/users" component={Users}></Route>
+      </BrowserRouter>
+    </div>
+  );
+};
+export default App;
---
+++ lesson13/hw1/src/Users.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Link, Switch, useParams } from "react-router-dom";
+
+import Product from "./User";
+
+const Users = ({ match }) => {
+  return (
+    <div className="page__content">
+      <h1>Users</h1>
+      <ul className="navigation">
+        <li className="navigation__item">
+          <Link to={`${match.url}/github`}>Github</Link>
+        </li>
+        <li className="navigation__item">
+          <Link to={`${match.url}/facebook`}>Facebook</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path={match.url}>
+          <span>Select a product please</span>
+        </Route>
+        <Route path={`${match.url}/:productId`}>
+          <Product />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+export default Users;
