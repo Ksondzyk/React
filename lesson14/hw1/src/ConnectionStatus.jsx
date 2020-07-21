a1d510e3b41f56b215960dd3292e47b253527ade
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-21 19:31:23 +0300

    push

--- lesson14/hw1/src/ConnectionStatus.jsx
+++ lesson14/hw1/src/ConnectionStatus.jsx
@@ -21,40 +21,4 @@ const ConnectionStatus = () => {
   );
 };
 
-// class ConnectionStatus extends Component {
-//   state = {
-//     status: true,
-//   };
-//   //
-//   componentDidMount() {
-//     window.addEventListener("online", this.setStatusOnline);
-//     window.addEventListener("offline", this.setStatusOffline);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener("online", this.setStatusOnline);
-//     window.removeEventListener("offline", this.setStatusOffline);
-//   }
-
-//   setStatusOnline = () => {
-//     this.setState({
-//       status: true,
-//     });
-//   };
-//   setStatusOffline = () => {
-//     this.setState({
-//       status: false,
-//     });
-//   };
-
-//   render() {
-//     const statusVisible = this.state.status ? "online" : "offline";
-//     const isRender = !this.state.status;
-//     return (
-//       <div className={`status ${isRender ? "status_offline" : null}`}>
-//         {statusVisible}
-//       </div>
-//     );
-//   }
-// }
 export default ConnectionStatus;
